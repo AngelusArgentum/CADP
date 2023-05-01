0f0a7c1ebaa9ac861fc1df5ba719908bc32373de
Mateo Matus <110347934+SkillFP@users.noreply.github.com>
2023-05-01 15:32:44 -0300

    Consignas

--- Practica 2 P2/Ejercicio10.pas
+++ Practica 2 P2/Ejercicio10.pas
@@ -1,5 +1,10 @@
 program Ejercicio10;
-
+{10. Realizar un programa modularizado que lea una secuencia de caracteres y verifique si cumple con el patrón
+A$B#, donde:
+- A es una secuencia de sólo letras vocales
+- B es una secuencia de sólo caracteres alfabéticos sin letras vocales
+- los caracteres $ y # seguro existen
+Nota: en caso de no cumplir, informar que parte del patrón no se cumplió.}
 Function EsVocal (c:char):boolean;
 begin
     EsVocal:=(c='a') OR (c='A') OR (c='e') OR (c='E') OR (c='i') OR (c='I') OR (c='o') OR (c='O') OR (c='u') OR (c='U');
