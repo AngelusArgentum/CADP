77169501f170952ecbd7d0c460d9a441942d4340
Mateo Matus <110347934+SkillFP@users.noreply.github.com>
2023-05-01 17:53:53 -0300

    Update Ejercicio5.pas

--- Practica 2 P2/Ejercicio5.pas
+++ Practica 2 P2/Ejercicio5.pas
@@ -1,13 +1,4 @@
 program Ejercicio5;
-{5. 
-a. Realizar un módulo que reciba un par de números (numA,numB) y retorne si numB es el doble de numA.
-
-b. Utilizando el módulo realizado en el inciso a., realizar un programa que lea secuencias de pares de
-números hasta encontrar el par (0,0), e informe la cantidad total de pares de números leídos y la cantidad de
-pares en las que numB es el doble de numA.
-
-Ejemplo: si se lee la siguiente secuencia: (1,2) (3,4) (9,3) (7,14) (0,0) el programa debe informar los valores
-4 (cantidad de pares leídos) y 2 (cantidad de pares en los que numB es el doble de numA).}
 function DobledeA (numA,numB:real):boolean;
     var
         Esdoble:boolean;
@@ -39,3 +30,12 @@ begin
     end;
     writeln('Se leyeron ',ParesT,' pares de números. En los cuales ',Pares2xA,' el segundo número era el doble del primero.');
 end.
+{5. 
+a. Realizar un módulo que reciba un par de números (numA,numB) y retorne si numB es el doble de numA.
+
+b. Utilizando el módulo realizado en el inciso a., realizar un programa que lea secuencias de pares de
+números hasta encontrar el par (0,0), e informe la cantidad total de pares de números leídos y la cantidad de
+pares en las que numB es el doble de numA.
+
+Ejemplo: si se lee la siguiente secuencia: (1,2) (3,4) (9,3) (7,14) (0,0) el programa debe informar los valores
+4 (cantidad de pares leídos) y 2 (cantidad de pares en los que numB es el doble de numA).}
